a894c16ad7db832a480069caabe15cdaf7c553c1
Loago Moremi <loagomoremi@gmail.com>
2025-08-26 19:10:19 +0200

    tried adding global font

--- brainwave/app/_layout.tsx
+++ brainwave/app/_layout.tsx
@@ -1,6 +1,18 @@
 import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import AppLoading from "expo-app-loading";
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    Poppins: require('../assets/fonts/Poppins-Regular.ttf'),
+    'Poppins-Bold': require('../assets/fonts/Poppins-Bold.ttf'),
+    'Poppins-Italic': require('../assets/fonts/Poppins-Italic.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
   <Stack>
     <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
---
+++ brainwave/components/AppText.tsx
@@ -0,0 +1,5 @@
+import { Text, TextProps } from "react-native";
+
+export default function AppText(props: TextProps){
+    return <Text {...props} style={[{fontFamily: 'Poppins-Regular'}, props.style]}>{props.children}</Text>
+}
