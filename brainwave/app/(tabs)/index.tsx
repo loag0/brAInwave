782e247cad49f45f6216f701671950baaa515bae
Loago Moremi <loagomoremi@gmail.com>
2025-08-26 19:10:19 +0200

    tried adding global font

--- brainwave/app/(tabs)/index.tsx
+++ brainwave/app/(tabs)/index.tsx
@@ -1,8 +1,10 @@
 import { Text, View, StyleSheet } from "react-native";
 import axios from "axios";
 import { useEffect } from "react";
+import AppText from "../../components/AppText";
 
 export default function Index() {
+    
   useEffect(() => {
     axios
       .get("http://127.0.0.1:8000/ping")
@@ -10,18 +12,21 @@ export default function Index() {
       .catch((err) => console.log(err));
   }, []); // runs once when component mounts
 
-  return (
-    <View style={styles.container}>
-      <Text>edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
-}
+    return (
+      <View style={styles.container}>
+        <AppText style={styles.apptext}>Edit app/index.tsx to edit this screen.</AppText>
+      </View>
+    );
+  }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#ffffff",
     justifyContent: "center",
-    alignItems: "center", // centers text horizontally
+    alignItems: "center",
   },
+  apptext: {
+    fontSize: 32
+  }
 });
