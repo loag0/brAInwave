7696a21a36327a064a636b85d5e4a6c3555e85be
Loago Moremi <loagomoremi@gmail.com>
2025-08-26 16:05:58 +0200

    added backend folder

--- brainwave/app/(tabs)/index.tsx
+++ brainwave/app/(tabs)/index.tsx
@@ -1,11 +1,18 @@
 import { Text, View, StyleSheet } from "react-native";
+import axios from "axios";
+import { useEffect } from "react";
 
 export default function Index() {
+  useEffect(() => {
+    axios
+      .get("http://127.0.0.1:8000/ping")
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err));
+  }, []); // runs once when component mounts
+
   return (
-    <View
-      style={styles.container}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text>edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
@@ -13,8 +20,8 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    display: "flex",
     backgroundColor: "#ffffff",
     justifyContent: "center",
-  }
-})
+    alignItems: "center", // centers text horizontally
+  },
+});
